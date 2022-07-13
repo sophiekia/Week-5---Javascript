14341f304eaba5f7f56ce4b291fd14e37777f592
Sophie Kia <107413111+sophiekia@users.noreply.github.com>
2022-07-13 16:56:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Hello my name is Andriod and I am');
+console.log(568 + 3.5);
+console.log('years old');
+
+console.log(3 + 4); // prints 7
+console.log(2022 - 1969)
+console.log(65 / 240)
+console.log(0.2708 * 123)
